954f63f5567d33ae9afba60aa9e6ddfc195026e2
Yang Wang <yangwang@Yangs-MacBook-Air.local>
2025-01-01 23:34:02 +0100

    add generating two random passwords function

--- index.js
+++ index.js
@@ -3,6 +3,31 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"
     "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
     "~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
 
+let firstPasswordEl = document.getElementById("password1")
+let secondPasswordEl = document.getElementById("password2")
 
+function getCharacter() {
+    let char = characters[Math.floor(Math.random() * characters.length)]
+    return char
+}
 
+function getRandomPasswordOne() {
+    let firstPassword = ""
+    for (let i = 0; i < 15; i++) {
+        firstPassword += getCharacter()
+    }
+    return firstPassword
+}
 
+function getRandomPasswordTwo() {
+    let secondPassword = ""
+    for (let i = 0; i < 15; i++) {
+        secondPassword += getCharacter()
+    }
+    return secondPassword
+}
+
+function showTwoPasswords() {
+    firstPasswordEl.textContent = getRandomPasswordOne()
+    secondPasswordEl.textContent = getRandomPasswordTwo()
+}
